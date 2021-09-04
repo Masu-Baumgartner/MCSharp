6a1427a2aa96d65c9003f599bdccbbbb586d7128
BauMar@jvfg-cham.schule <marcel.kbkm@gmail.com>
2021-09-04 19:11:40 +0200

    Added MinecraftCLient class

---
+++ MCSharp/API/MinecraftClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCSharp.API
+{
+    class MinecraftClient
+    {
+    }
+}
--- MCSharpTest/Program.cs
+++ MCSharpTest/Program.cs
@@ -6,7 +6,7 @@ namespace MCSharpTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
