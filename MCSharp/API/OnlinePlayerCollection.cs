14dc07c4098b92568a93cd97896698ed2da53dc4
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-09-04 19:41:35 +0200

    started collection

---
+++ MCSharp/API/OnlinePlayerCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCSharp.API
+{
+    public class OnlinePlayerCollection : InternalCollection<OnlinePlayer>
+    {
+        public OnlinePlayerCollection()
+        {
+
+        }
+    }
+}
