2d09254db242c56da3d4bbfb066e62701f3612e7
Dalk-GitHub <67603460+Dalk-GitHub@users.noreply.github.com>
2021-12-12 13:46:10 +0100

    Update IMaterial.cs

--- MCSharp/Utils/Data/IMaterial.cs
+++ MCSharp/Utils/Data/IMaterial.cs
@@ -4,7 +4,44 @@ using System.Text;
 
 namespace MCSharp.Utils.Data
 {
-    internal class IMaterial
-    {
-    }
+	public interface IMaterial
+	{
+		string SoundCategory { get; }
+		IMaterial SetSoundCategory(string soundCategory);
+
+		IMapColor MapColorValue { get; }
+
+		TintType TintType { get; }
+		Color TintColor { get; }
+		IMaterial SetTintType(TintType type, Color color);
+
+		double Slipperiness { get; }
+
+		IMaterial SetSlipperines(double value);
+
+		float Hardness { get; }
+		IMaterial SetHardness(float hardness);
+
+		bool BlocksLight { get; }
+		bool BlocksMovement { get; }
+		IMaterial SetTranslucent();
+		IMaterial SetRequiresTool();
+		IMaterial SetBurning();
+		bool CanBurn { get; }
+		bool IsLiquid { get; }
+		bool IsOpaque { get; }
+		bool IsReplaceable { get; }
+		bool IsSolid { get; }
+		bool IsToolRequired { get; }
+		IMaterial SetReplaceable();
+
+		bool IsWatterLoggable { get; }
+
+		IMaterial SetWaterLoggable();
+
+		bool CanUseTool(ItemType type, ItemMaterial material);
+		IMaterial SetRequiredTool(ItemType type, ItemMaterial material);
+
+		IMaterial Clone();
+	}
 }
