7a7207853e767d8dea5b5a22a635ad2066f976b5
Marcel Baumgartner <marcel.kbkm@gmail.com>
2022-02-09 16:32:07 +0100

    Setup testing

--- MCSharp Client Test/Program.cs
+++ MCSharp Client Test/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MCSharp_Client_Test
+﻿using MCSharp.Network;
+using MCSharp.Pakets.Server.Handshake;
+using MCSharp.Pakets.Server.Login;
+
+using System.Net.Sockets;
+
+namespace MCSharp_Client_Test
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            TcpClient cl = new TcpClient("127.0.0.1", 25565);
 
+            var connection = new MinecraftConnection(cl, MCSharp.Enums.MinecraftFlow.ClientToServer);
+
+            PaketRegistry writer = new PaketRegistry();
+            PaketRegistry.RegisterClientPakets(writer);
+
+            PaketRegistry reader = new PaketRegistry();
+            PaketRegistry.RegisterServerPakets(reader);
+
+            connection.WriterRegistry = writer;
+            connection.ReaderRegistry = reader;
+
+            connection.Handler = new TestHandler()
+            {
+                Connection = connection
+            };
+
+            connection.Start();
+
+            connection.State = MCSharp.Enums.MinecraftState.Login;
+
+            connection.SendPaket(new HandshakePaket()
+            {
+                NextState = 2,
+                ProtocolVersion = 757,
+                ServerAddress = "127.0.0.1",
+                ServerPort = 25565
+            });
+
+            connection.SendPaket(new LoginStartPaket()
+            {
+                Username = "MCSharpClient"
+            });
         }
     }
 }
--- MCSharp Client Test/TestHandler.cs
+++ MCSharp Client Test/TestHandler.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Logging.Net;
+
+using MCSharp.Network;
+using MCSharp.Pakets;
+using MCSharp.Pakets.Client.Login;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,38 @@ using System.Threading.Tasks;
 
 namespace MCSharp_Client_Test
 {
-    internal class TestHandler
+    public class TestHandler : IPaketHandler
     {
+        public MinecraftConnection Connection { get; set; }
+
+        public void Handshake(IPaket paket)
+        {
+            
+        }
+
+        public void Login(IPaket paket)
+        {
+            if(paket is SetCompressionPaket compressionPaket)
+            {
+                Connection.CompressionThreshold = compressionPaket.Threshold;
+                Connection.CompressionEnabled = true;
+            }
+
+            if(paket is LoginSuccessPaket loginSuccessPaket)
+            {
+                Connection.State = MCSharp.Enums.MinecraftState.Play;
+                Logger.Info("Login success");
+            }
+        }
+
+        public void Play(IPaket paket)
+        {
+            
+        }
+
+        public void Status(IPaket paket)
+        {
+            
+        }
     }
 }
